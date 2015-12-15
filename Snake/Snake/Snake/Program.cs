690c736aeff51ce1a49a10eff8a257962d31b64b
mishaelll <chikotijlo@gmail.com>
2015-12-15 12:35:25 +0500

    урок 7. Горизонтальная линия

--- Snake/Snake/Snake/Program.cs
+++ Snake/Snake/Snake/Program.cs
@@ -10,25 +10,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
-
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
+            
+            HorizontalLine line = new HorizontalLine(0,Console.WindowWidth-1,0,'*');
+            line.Draw();
+            
 
             Console.ReadLine();
         }
